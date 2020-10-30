ab35ea1e23bd69d28e0747d3961329022fc1f9d8
bartekpaczek <bartek.paczek@gmail.com>
2020-10-30 11:00:58 +0000

    Zad 3

--- lab1.cpp
+++ lab1.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <math.h>
+struct Informer
+{
+    Informer() { std::cout << "Construct inner\n"; };
+
+    ~Informer() { std::cout << "Destruct inner\n"; }
+};
+
 struct Wektor2D
 {
-    double x;
-    double y;
+    Wektor2D(double x_val, double y_val)
+    {
+        x = x_val;
+        y = y_val;
+        std::cout << "x=" << x << " "
+                  << "y=" << y << "\n";
+    }
+    ~Wektor2D()
+    {
+        std::cout << "Destruct "
+                  << "x=" << x << " "
+                  << "y=" << y << "\n";
+    }
+    double   x;
+    double   y;
+    Informer info1;
 
     double norm() { return sqrt(x * x + y * y); }
 };
 
 int main()
 {
-    Wektor2D dupa;
-    dupa.x = 2;
-    dupa.y = 2;
-    std::cout << dupa.norm() << std::endl;
+    Wektor2D dupa1{1, 1};
+    Wektor2D dupa2{2, 2};
+    Wektor2D dupa3{3, 3};
 }
