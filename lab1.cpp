f77ab53c37ce3904d66e67826f212dff20422250
bartekpaczek <bartek.paczek@gmail.com>
2020-10-30 08:32:24 +0000

    zad1

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <math.h>
+struct Wektor2D
+{
+    double x;
+    double y;
+
+    double norm() { return sqrt(x * x + y * y); }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D dupa;
+    dupa.x = 2;
+    dupa.y = 2;
+    std::cout << dupa.norm() << std::endl;
 }
